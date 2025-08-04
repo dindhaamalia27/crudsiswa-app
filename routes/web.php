d4f07789bffbfd479915466fa66f56e38ed5aef4
dindhaamalia27 <punyandaa290@gmail.com>
2025-08-04 10:00:59 +0700

    sudah mengerjakan create dan memperbarui siswa

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiswaController;
 
-Route::get('siswa/create', function () {
-    return view('siswa.create');
-});
-Route::get('/', function () {
-    return view('siswa.index');
-});
+Route::get('/',[SiswaController::class, 'index']);
+
+Route::get('/siswa/create',[SiswaController::class, 'create']);
+
 Route::post('/siswa/store',[SiswaController::class,'store']);
